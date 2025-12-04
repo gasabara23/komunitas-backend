d407eaf4a4ec6ebce53ae128ec65d94175624651
gasabara23 <galerighaza15@gmail.com>
2025-12-04 11:58:10 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js";
+import commentRoutes from "./routes/comments.js";
+import uploadRoutes from "./routes/upload.js";
+dotenv.config();
+connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+app.use("/api/auth", authRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/upload", uploadRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
